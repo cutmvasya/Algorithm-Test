791f284e5e775dffff1a5740b85021b1eb769b07
cutmvasya <meutia.vasya@gmail.com>
2021-11-10 20:11:13 +0700

    algorithm test

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,3 @@
-// - averagePair([-1, 0, , 3, 4, 5, 6], 4.1) => false
-// - averagePair([1, 2, 3], 2.5) => true
-// - averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8) => true
-
 function averagePair(num1, num2) {
     for (let i = 0; i < num1.length; i++) {
         for (let j = i + 1; j < num1.length; j++) {
